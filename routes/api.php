e4d1a0b52355352cd192f3a6894feaeca8e6e708
aaquinonez2808 <aaquinonez1@espe.edu.ec>
2023-01-06 00:26:10 -0500

    rutas api

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/teams', [App\Http\Controllers\TeamController::class, 'list']);
 
+Route::post('/teams', [App\Http\Controllers\TeamController::class, 'store']);
+
+Route::get('/teams/{id}', [App\Http\Controllers\TeamController::class, 'show']);
+
+Route::put('/teams/{id}', [App\Http\Controllers\TeamController::class, 'update']);
+
+Route::delete('/teams/{id}', [App\Http\Controllers\TeamController::class, 'delete']);
+
+Route::get('/games/inscriptionTotal', [App\Http\Controllers\GameController::class, 'CountInscripcionesByGame']);
+
+Route::get('/games/inscriptionMax', [App\Http\Controllers\GameController::class, 'MaxInscritoByGame']);
+
+Route::get('/games', [App\Http\Controllers\GameController::class, 'list']);
+
+Route::get('/games/categoria/{id}', [App\Http\Controllers\GameController::class, 'listByCategory']);
+
+Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'list']);
