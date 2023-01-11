4b8771f203acca2dea0d5ac36c86e3eafe579c19
Tomyrp <tarios@espe.edu.ec>
2023-01-10 23:49:09 -0500

    tom

--- resources/views/pdf/games.blade.php
+++ resources/views/pdf/games.blade.php
@@ -1,27 +1,36 @@
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 <script src="{{ mix('js/app.js') }}" defer></script>
 
+<table class="table table-striped">
+
+    <thead>
+        <tr>
+            <th>---------------------------------------------------------------</th>
+        </tr>
+
+        <tr>
+            <th>------------------------Lista de juegos-------------</th>
+        </tr>
+        
+        <tr>
+            <td>Numero</td>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+
+        </tr>
 
 
+    </thead>
 
-            <table class="table table-striped"  >
-                <thead >
-                    <tr>
-                        <td>#</td>
-                        <th>Nombre</th>
-                    
-                        <td>Numero de juegos</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($games as $row)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $row->nombre }}</td>
-                        
-                        <td>{{ $row->id_juegos }}</td>
-                    </tr>
-                        @endforeach
-                </tbody>
+    <tbody>
 
-            </table>
+        @foreach($games as $row)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $row->nombre }}</td>
+            <td>{{ $row->id_juegos }}</td>
+        </tr>
+        @endforeach
+    </tbody>d
+
+</table>
