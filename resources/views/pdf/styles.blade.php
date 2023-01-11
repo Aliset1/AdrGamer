469e52af17e3db7ecf477d9a5b25d1e4528add77
aaquinonez2808 <aaquinonez1@espe.edu.ec>
2023-01-11 14:07:42 -0500

    cambios casuales, sin css

---
+++ resources/views/pdf/styles.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Teams</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body>
+
+</body>
