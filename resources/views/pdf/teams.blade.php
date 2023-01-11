c660659a73a012fdf40eecbe13f3a0d41c69e74a
Aliset1 <alvargas3@espe.edu.ec>
2023-01-11 15:28:41 -0500

    reportes

--- resources/views/pdf/teams.blade.php
+++ resources/views/pdf/teams.blade.php
@@ -1,7 +1,20 @@
+<!doctype html>
+<html lang="es">
 
-    <div class="container">
+<head>
+    <title>Laravel</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        table {
+            font-size: 12px;
+        }
+    </style>
+</head>
+<div class="container">
         <div class="text-center text-red-500">
-            <h1 class="text-2xl font-bold text-center">Reporte de las Inscripciones Grupales </h1>
+            <h1 class="text-2xl font-bold text-center">Listado de Equipos</h1>
         </div>
         <table>
             <thead>
@@ -13,9 +26,14 @@
                 </tr>
             </thead>
             <tbody>
-                <!-- Mostrar datos en orden alfabetico -->
-                @
+                @foreach($teams as $row)
+                <tr>
+                    <td class="border border-gray-400 px-4 py-2">{{ $loop->iteration }}</td>
+                    <td class="border border-gray-400 px-4 py-2">{{ $row->team }}</td>
+                    <td class="border border-gray-400 px-4 py-2">{{$row->game}}</td>
+                    <td class="border border-gray-400 px-4 py-2">{{$row->date}}</td>
+                </tr>
+                    @endforeach
             </tbody>
         </table>
     </div>
-
